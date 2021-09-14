c62dd72bc279f4ca1407118999f7ba8affc18ba9
Jesus-Alva <jesusalva575@gmail.com>
2021-09-13 21:07:36 -0500

    migracion de la tabla autor

--- database/migrations/2021_09_14_020209_create_autor_table.php
+++ database/migrations/2021_09_14_020209_create_autor_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class NuevaTablaCategorias extends Migration
+class CreateAutorTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,11 @@ class NuevaTablaCategorias extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('autor', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +27,6 @@ class NuevaTablaCategorias extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('autor');
     }
 }
